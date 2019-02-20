118624913cec6d4a6ea29816d92f70ffd2fad466
oliver <oflatt@gmail.com>
2019-02-20 14:58:06 -0700

    fix kwargs for new

--- pythonDestructiveClass/DestructiveClass.py
+++ pythonDestructiveClass/DestructiveClass.py
@@ -4,12 +4,12 @@ from DestructiveBox import DestructiveBox
 # a class like FrozenClass but packages the object in a DestructiveBox
 class DestructiveClass(object):
     
-    def __new__(*args):
+    def __new__(*args, **kwargs):
         # create it using new
         o = super(DestructiveClass, args[0]).__new__(args[0])
         
         # initiate the object (self is the first arg, do not include it)
-        o.__init__(*args[1:])
+        o.__init__(*args[1:], **kwargs)
         
         # wrap the item in a destructive box
         return DestructiveBox(o)
